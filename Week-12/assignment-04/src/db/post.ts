b385538f91a7972e49af2c78c8b3c843a705df6e
gandhamphotos@gmail.com <gandhamphotos@gmail.com>
2026-04-26 19:07:00 +0000

    assignment-04

--- Week-12/assignment-04/src/db/post.ts
+++ Week-12/assignment-04/src/db/post.ts
@@ -1,13 +1,39 @@
 import { client } from '../index';
+import {QueryResult} from "pg";
 
+interface Post{
+id:number;
+user_id:number;
+content:string;
+created_at:number
+}
+
+interface Feed{
+	id:number;
+	content:string;
+	created_at:string;
+	user_name:string;
+	like_count:number
+}
 export async function createPost(userId: number, content: string) {
- 
+ const query = "insert into posts(user_id,content) values($1,$2) returning *"
+ const values = [userId,content];
+ const res:QueryResult<Post> = await client.query(query,values);
+ const post = res.rows[0];
+ return post;
 }
 
 export async function likePost(userId: number, postId: number) {
- 
+  const query = "insert into likes(user_id,post_id) values($1,$2) on conflict(user_id,post_id) do nothing returning *";
+  const values = [userId,postId];
+  const res:QueryResult<Post> = await client.query(query,values);
+  const like = res.rows[0];
+  return like;
 }
 
 export async function getFeed() {
-
+	const query = " select p.id as id ,p.content as content ,p.created_at as created_at ,u.username as username ,count(l.post_id) as like_count from users u join posts p on p.user_id = u.id left join likes l on l.post_id = p.id group by u.username,p.id";
+      	const res:QueryResult<Feed> = await client.query(query);
+ const feed = res.rows;
+ return feed;
 }
--- Week-12/assignment-04/src/db/user.ts
+++ Week-12/assignment-04/src/db/user.ts
@@ -1,9 +1,25 @@
 import { client } from '../index';
+import {QueryResult } from "pg";
+
+interface User{
+ id : number;
+ username:string;
+ password:string;
+ name:string
+}
 
 export async function createUser(username: string, password: string, name: string) {
-  
+   const query = "insert into users(username,password,name) values($1,$2,$3) returning *";
+   const values = [username,password,name];
+   const res:QueryResult<User> = await client.query(query,values);
+   const user = res.rows[0];
+   return user;   
 }
 
 export async function getUser(id: number) {
-  
+ const query = "select * from users where id = $1";
+ const values = [id];
+ const res:QueryResult<User> = await client.query(query,values);
+ const users = res.rows;
+ return users; 
 }
